d33ca24b5ec0836662309cac28d0c1124e8639f5
pyama2000 <takahiko-yamashita@retty.me>
2023-08-09 23:05:15 +0900

    feat: establish server

--- server-with-envoy-and-keycloak/server/src/main.rs
+++ server-with-envoy-and-keycloak/server/src/main.rs
@@ -1,4 +1,38 @@
+use axum::http::StatusCode;
+use axum::routing::get;
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let app = axum::Router::new().route("/healthz", get(|| async { StatusCode::OK }));
+    let port = std::env::var("SERVER_PORT").unwrap_or_else(|_| "8080".to_string());
+    let addr = format!("0.0.0.0:{}", port).parse()?;
+    println!("listening on: {}", &addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
+        .await?;
+
+    Ok(())
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .unwrap_or_else(|e| panic!("failed to install Ctrl+C handler: {}", e))
+    };
+
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .unwrap_or_else(|e| panic!("failed to install singal handler: {}", e))
+            .recv()
+            .await;
+    };
+
+    tokio::select! {
+        _ = ctrl_c => println!("receive ctrl_c signal"),
+        _ = terminate => println!("receive terminate"),
+    }
+
+    println!("signal received, starting graceful shutdown");
 }
