93ba5ae30f5e4ee8d8787b4f333ad2af98f4171c
pyama2000 <takahiko-yamashita@retty.me>
2023-08-12 12:59:57 +0900

    change: client code を生成しない

--- grpc-server-with-envoy/server/build.rs
+++ grpc-server-with-envoy/server/build.rs
@@ -5,6 +5,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .file_descriptor_set_path(out_dir.join("auth_service_descriptor.bin"))
+        .build_client(false)
         .compile(
             &[format!("{}/auth/v1/auth_service.proto", PROTO_ROOT_DIR)],
             &[PROTO_ROOT_DIR],
@@ -12,6 +13,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .file_descriptor_set_path(out_dir.join("misc_service_descriptor.bin"))
+        .build_client(false)
         .compile(
             &[format!("{}/misc/v1/misc_service.proto", PROTO_ROOT_DIR)],
             &[PROTO_ROOT_DIR],
